07351f1ffcbc89a0f801240ce1d43ce13f4a82f8
Hayk An <say@hihayk.com>
2020-10-25 00:18:51 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    artist: {
+      name: 'some artist name',
+      bio: 'some bio',
+      picture: 'pic.png',
+      releases: [
+        {
+          title: 'some release title',
+          description: 'some description',
+          images: [
+            'front.png',
+            'back.png',
+          ]
+        }
+      ]
+    },
+  }
+]
