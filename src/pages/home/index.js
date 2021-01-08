a3044343b4c179e3fd2e28d43bb075634d67247d
Hayk An <say@hihayk.com>
2021-01-09 00:26:05 +0100

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -141,16 +141,15 @@ const HomePage = () => {
           </NewsSection>
           <NewsletterSection>
             <Text size="l" color="red">
-              subscribe to Hedonic <br />Reversal newsletter
+              subscribe to Hedonic <br />
+              Reversal newsletter
             </Text>
           </NewsletterSection>
         </EndSection>
       </ExploreSection>
       <DistributionSection>
         <Text align="center">
-          Hedonic Reversal is distributed worldwide
-          <br />
-          exclusively by <Text display="inline" color="red">Envelope Estructure</Text>
+          Hedonic Reversal is distributed worldwide exclusively by <Text display="inline" color="red">Envelope Estructure</Text>
         </Text>
       </DistributionSection>
     </>
