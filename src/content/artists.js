4fb4a418ca3bbfa293090e5b11ea7bf5f04578ae
Hayk An <say@hihayk.com>
2021-06-11 20:47:41 +0200

    Update artists.js

--- src/content/artists.js
+++ src/content/artists.js
@@ -41,6 +41,46 @@ export const artistsContent = [
   },
   // --------------------------------------------------------
   {
+    name: 'Miguel',
+    bio: [
+      'Tio bueno. Muy guay',
+    ],
+    picture: 'blume.jpg',
+    links: {
+      facebook: 'https://www.facebook.com/rrefectori/',
+      soundcloud: 'https://www.facebook.com/rrefectori/',
+      instagram: 'https://www.facebook.com/rrefectori/',
+    },
+    releases: [
+      {
+        id: 1,
+        albumId: '466129291',
+        title: 'Temazos vol1',
+        catNumber: 'HEDREV-00004',
+        releaseDate: '21 June 2019',
+        format: 'Vinyl / Digital',
+        buyUrl: 'https://hedonicreversal.bandcamp.com/album/espleni',
+        credits: [
+          'Produced by Xavier Longàs',
+          'Mastered by Pedro Pina',
+          'Art direction & Design Miguel Sueiro',
+          'Photograhpy by Eva Paià',
+        ],
+        description: [
+          'From the physical transformation of organic elements that melt generating new textures, Espleni is created. Fragmented into nine pieces, it starts with an inmersive melody starting a route without return where you can explore frozen territories of heavy structures. This reflects the union between body and raw material, an interest that was already glimpsed in its first release with Bestiarie (2015). The distortion of organic sounds arrives to wide landscapes of noise, sometimes moderated and in others uncontrolled. Noises that are juxtaposed in a constant dialogue until its definitive bifurcation.',
+          'This project starts from the recording of everyday sounds that surround the author, using a Zoom H5 recorder, with which he later experiments, manipulating and gathering them until creating a musically harmonic composition.',
+        ],
+        images: [
+          'back-vinyl.gif',
+          'back.gif',
+          '0022784123_10.jpg',
+        ],
+        coverImage: 'cover.jpg',
+      },
+    ],
+  },
+  // --------------------------------------------------------
+  {
     name: 'Orphan Swords',
     bio: [
       'Refectori (Xavier Longàs) has performed in different halls and festivals in Barcelona as Bicefal festival, also in the third and fourth WAV Conference that were celebrated in El Pumarejo and in Hangar respectively, likewise took sounds next to techno in Kreuzberg Club organized by Diffuse Reality and with the HEX collective has participated in his second episode in The Loft of Razzmatazz.',
