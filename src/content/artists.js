3448883c7da55749666ce1e8562f42849354f3d2
miguelsueiro <73364404+miguelsueiro@users.noreply.github.com>
2021-06-14 18:26:40 +0200

    Update artists.js
    Huma info releases uploaded

--- src/content/artists.js
+++ src/content/artists.js
@@ -4,7 +4,7 @@ export const artistsContent = [
     bio: [
       'Huma is the personal project of Andrés Satué. Born in Galicia but based in Barcelona, he has an extensive career in musical experimentation, from his first math-kraut-rock and prog bands, to experimental electronics with his previous project Esquelas and now with Huma. He has released several albums under labels including Bestiarie, ENSMBLE and Matapadre. He participated in the Red Bull Music Academy Bass Camp in Madrid, played at some of the most relevant Spanish experimental electronics festivals and is co-founder of Hedonic Reversal label and Bicefal Festival. He also runs a monthly radio show with his label partner Miguel Sueiro at Dublab Barcelona.',
     ],
-    picture: 'blume.jpg',
+    picture: 'huma.jpg',
     links: {
       facebook: 'https://www.facebook.com/Huma4444',
       soundcloud: 'https://soundcloud.com/huma4444',
@@ -23,6 +23,7 @@ export const artistsContent = [
           'Written and produced by Andrés Satué',
           'Mastered by Pedro Pina',
           'Art direction and design by Miguel Sueiro',
+          '© 2016 www.hedonicreversal.com',                     
         ],
         description: [
           'Huma is the personal project of Andrés Satué, half of the duo Esquelas, project in which they released “Inciso” (Ensmble) and more recently “Un pozo lleno de anzuelos” (Bestiarie). He was participant in the RedBull Muisc Academy BassCamp in Madrid 2015. He is also cofounder of Versal and Bicefal.',
@@ -32,12 +33,114 @@ export const artistsContent = [
           '«The intersection between extreme, saturation and depth is what describes this new project of Andrés Satué ...» — Frankie Pizá, This Is Underground',
         ],
         images: [
-          'back-vinyl.gif',
-          'back.gif',
+          'huma-tresfases-00.jpg',
+          'huma-tresfases-01.jpg',
+          'huma-tresfases-02.jpg',
+          'huma-tresfases-03.jpg',
+          'huma-tresfases-04.jpg',
+          'huma-tresfases-05.jpg',          
           '0022784123_10.jpg',
         ],
-        coverImage: 'cover.jpg',
+        coverImage: 'huma-tresfases-cover.jpg',
       },
+      {
+        id: 2,
+        albumId: '2733975405',
+        title: 'Yocto / Yotta',
+        catNumber: 'HEDREV-00002',
+        releaseDate: '10 March 2018',
+        format: 'Vinyl / Digital',
+        buyUrl: 'https://hedonicreversal.bandcamp.com/album/yocto-yotta',
+        credits: [
+          'Written and produced by Andrés Satué',
+          'Mastered by Pedro Pina',
+          'Art direction and design by Miguel Sueiro',
+          '© 2018 www.hedonicreversal.com',                      
+        ],
+        description: [
+          'Two new tracks after Humas 2016 first EP «Las tres fases del movimiento',
+          'Yocto (symbol y) is a prefix of the International System that indicates a factor of 10-24.',
+          'Yotta (symbol Y) is a prefix of the International System that indicates a factor of 1024 (a quadrillion).',
+        ],
+        images: [
+          'huma-yocto-00.jpg',
+        ],
+        coverImage: 'huma-yocto-cover.jpg',
+      },    
+      {
+        id: 3,
+        albumId: '3277444269',
+        title: 'Eva',
+        catNumber: 'HEDREV-00006',
+        releaseDate: '11 November 2019',
+        format: 'Cassette / Digital',
+        buyUrl: 'https://hedonicreversal.bandcamp.com/album/eva-2',
+        credits: [
+          'Written and produced by Andrés Satué',
+          'Mastered by Matt Colton at Alchemy Mastering',
+          'Art direction and design by Miguel Sueiro',
+          'Cover illustration by Martina Ampuero',
+          '© 2019 www.hedonicreversal.com',                     
+        ],
+        description: [
+          'Eva is a journey from the birth of humanity (Adán) to the rebirth of the new human (Eva). With a positive but disturbing point of view it delves into the idea of technological singularity and the consequences of the exponential growth of human development. In order to do so, it explores in an extreme and profound way the relationships between concepts such as micro/macro, organic/inorganic, natural/artificial, man/machine.',
+        ],
+        images: [
+          'huma-eva-00.jpg',
+          'huma-eva-01.jpg',
+          'huma-eva-02.jpg',
+          'huma-eva-03.jpg',
+          'huma-eva-04.jpg',          
+        ],
+        coverImage: 'huma-eva-cover.jpg',
+      },  
+      {
+        id: 4,
+        albumId: '695194201',
+        title: 'Eva Remixes',
+        catNumber: 'HEDREV-00006b',
+        releaseDate: '26 November 2019',
+        format: 'Cassette / Digital',
+        buyUrl: 'https://hedonicreversal.bandcamp.com/album/eva-remixes',
+        credits: [
+          'Written and produced by Andrés Satué',
+          'Mastered by Matt Colton at Alchemy Mastering',
+          'Art direction and design by Miguel Sueiro',
+          'Cover illustration by Martina Ampuero',          
+          '© 2019 www.hedonicreversal.com',             
+        ],
+        description: [
+          'Eva is a journey from the birth of humanity (Adán) to the rebirth of the new human (Eva). With a positive but disturbing point of view it delves into the idea of technological singularity and the consequences of the exponential growth of human development. In order to do so, it explores in an extreme and profound way the relationships between concepts such as micro/macro, organic/inorganic, natural/artificial, man/machine.',
+        ],
+        images: [
+          'huma-evaremixes-00.jpg',
+        ],
+        coverImage: 'huma-evaremixes-cover.jpg',
+      },     
+      {
+        id: 5,
+        albumId: '1673263714',
+        title: 'Emergence',
+        catNumber: 'HEDREV-00009',
+        releaseDate: '25 February 2021',
+        format: 'Cassette / Digital',
+        buyUrl: 'https://hedonicreversal.bandcamp.com/album/emergence',
+        credits: [
+          'Written and produced by Andrés Satué',
+          'Mastered by Pedro Pina',
+          'Art direction and design by Miguel Sueiro',
+          'Cover illustration by Mitrilo',          
+          'Inner sleeve graphics created with the Wolfram Language (www.wolfram.com)',   
+          '© 2021 www.hedonicreversal.com',             
+        ],
+        description: [
+          'Emergence experiments with irregular and unpredictable structures that arise from sequences and textures colliding and merging into rhythmic and melodic sonic explorations that seem non-human-like but at the same time intuitive and almost familiar. Inspired by the Emergence Theory, which states that a large number of relatively simple elements can self-organize into behaviors that appear to be unexpectedly complex, it creates a process by which musical order seems to emerge out from chaos.',
+        ],
+        images: [
+          'huma-emergence-00.jpg',
+        ],
+        coverImage: 'huma-emergence-cover.jpg',
+      },           
     ],
   },
   // --------------------------------------------------------
@@ -54,7 +157,7 @@ export const artistsContent = [
     },
     releases: [
       {
-        id: 1,
+        id: 0000671,
         albumId: '466129291',
         title: 'Temazos vol1',
         catNumber: 'HEDREV-00004',
@@ -89,7 +192,7 @@ export const artistsContent = [
     picture: 'orphan-swords.jpg',
     releases: [
       {
-        id: 2,
+        id: 56757,
         albumId: '466129291',
         title: 'Another',
         catNumber: 'HEDREV-00004',
@@ -124,7 +227,7 @@ export const artistsContent = [
     picture: 'orphan-swords.jpg',
     releases: [
       {
-        id: 3,
+        id: 45754756,
         albumId: '466129291',
         title: 'Just Like You',
         catNumber: 'HEDREV-00004',
