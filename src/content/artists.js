deb8a2b51fd6cce4a5041add3067d2655de9cfff
miguelsueiro <73364404+miguelsueiro@users.noreply.github.com>
2021-06-14 16:34:33 +0200

    Update artists.js
    Huma description
    Huma Las tres fases del movimiento

--- src/content/artists.js
+++ src/content/artists.js
@@ -1,34 +1,35 @@
 export const artistsContent = [
   {
-    name: 'Refectori',
+    name: 'Huma',
     bio: [
-      'Refectori (Xavier Longàs) has performed in different halls and festivals in Barcelona as Bicefal festival, also in the third and fourth WAV Conference that were celebrated in El Pumarejo and in Hangar respectively, likewise took sounds next to techno in Kreuzberg Club organized by Diffuse Reality and with the HEX collective has participated in his second episode in The Loft of Razzmatazz.',
+      'Huma is the personal project of Andrés Satué. Born in Galicia but based in Barcelona, he has an extensive career in musical experimentation, from his first math-kraut-rock and prog bands, to experimental electronics with his previous project Esquelas and now with Huma. He has released several albums under labels including Bestiarie, ENSMBLE and Matapadre. He participated in the Red Bull Music Academy Bass Camp in Madrid, played at some of the most relevant Spanish experimental electronics festivals and is co-founder of Hedonic Reversal label and Bicefal Festival. He also runs a monthly radio show with his label partner Miguel Sueiro at Dublab Barcelona.',
     ],
     picture: 'blume.jpg',
     links: {
-      facebook: 'https://www.facebook.com/rrefectori/',
-      soundcloud: 'https://www.facebook.com/rrefectori/',
-      instagram: 'https://www.facebook.com/rrefectori/',
+      facebook: 'https://www.facebook.com/Huma4444',
+      soundcloud: 'https://soundcloud.com/huma4444',
+      instagram: 'https://www.instagram.com/huma_4444',
     },
     releases: [
       {
         id: 1,
-        albumId: '466129291',
-        title: 'Espleni',
-        catNumber: 'HEDREV-00004',
-        releaseDate: '21 June 2019',
+        albumId: '1635759303',
+        title: 'Las tres fases del movimiento',
+        catNumber: 'HEDREV-00001',
+        releaseDate: '17 November 2016',
         format: 'Vinyl / Digital',
-        buyUrl: 'https://hedonicreversal.bandcamp.com/album/espleni',
+        buyUrl: 'https://hedonicreversal.bandcamp.com/album/las-tres-fases-del-movimiento',
         credits: [
-          'Produced by Xavier Longàs',
+          'Written and produced by Andrés Satué',
           'Mastered by Pedro Pina',
-          'Art direction & Design Miguel Sueiro',
-          'Photograhpy by Eva Paià',
+          'Art direction and design by Miguel Sueiro',
         ],
         description: [
-          'From the physical transformation of organic elements that melt generating new textures, Espleni is created. Fragmented into nine pieces, it starts with an inmersive melody starting a route without return where you can explore frozen territories of heavy structures. This reflects the union between body and raw material, an interest that was already glimpsed in its first release with Bestiarie (2015). The distortion of organic sounds arrives to wide landscapes of noise, sometimes moderated and in others uncontrolled. Noises that are juxtaposed in a constant dialogue until its definitive bifurcation.',
-          'This project starts from the recording of everyday sounds that surround the author, using a Zoom H5 recorder, with which he later experiments, manipulating and gathering them until creating a musically harmonic composition.',
-          'He has performed in different halls and festivals in Barcelona as Bicefal festival, also in the third and fourth WAV Conference that were celebrated in El Pumarejo and in Hangar respectively, likewise took sounds next to techno in Kreuzberg Club organized by Diffuse Reality and with the HEX collective has participated in his second episode in The Loft of Razzmatazz.',
+          'Huma is the personal project of Andrés Satué, half of the duo Esquelas, project in which they released “Inciso” (Ensmble) and more recently “Un pozo lleno de anzuelos” (Bestiarie). He was participant in the RedBull Muisc Academy BassCamp in Madrid 2015. He is also cofounder of Versal and Bicefal.',
+          'This is his first EP as Huma. “Las tres fases del movimiento" is based on two ideas, on one hand the three phases of movement of Meyerhold biomechanics, and on the other hand the three phases of movement in bodybuilding. Joining these two ideas he generates a very physical, visceral and emotional journey.',
+          '«A visceral and emotion-laden situation that sometimes seems to set in front of the abyss, but the most important thing is that it generates a movement.» —David Gozálvez, Neonized',
+          '"... Huma manages to make his music a purely corporeal act: his cuts are like dancers who move in complex and different ways in space. Sometimes they move as if they want to touch the sky ... » — Fantastic Plastic',
+          '«The intersection between extreme, saturation and depth is what describes this new project of Andrés Satué ...» — Frankie Pizá, This Is Underground',
         ],
         images: [
           'back-vinyl.gif',
