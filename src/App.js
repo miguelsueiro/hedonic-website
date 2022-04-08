c78fade2382d0a330d30775a888a746dee3ccff8
hihayk <say@hihayk.com>
2022-04-08 20:33:37 +0200

    noise

--- src/App.js
+++ src/App.js
@@ -26,9 +26,30 @@ const getReleasesContent = () => {
   return result
 }
 
+const Noise = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(https://media4.giphy.com/media/xWlgutH3ZZF6w/giphy.gif?cid=ecf05e47in6pfwkljzhnub9zd5i95siqcdlny4x6p85g16cu&rid=giphy.gif&ct=g)`,
+        position: "fixed",
+        top: "0",
+        left: "0",
+        width: "100%",
+        height: "100%",
+        pointerEvents: "none",
+        zIndex: "1",
+        '--baseFrequency': 3.33,
+        mixBlendMode: 'soft-light',
+        opacity: 0.1,
+      }}
+    />
+  )
+}
+
 const App = () => {
   return (
     <Router>
+<<<<<<< Updated upstream
       <div>
         <nav>
           <ul>
@@ -56,6 +77,52 @@ const App = () => {
                 picture={dataItem.picture}
                 releases={dataItem.releases}
               />
+=======
+      <BaseStyles />
+      <Noise />
+      <PageWrapper>
+        <MainWrapper>
+          <Header menuTop="70vh" />
+          <Switch>
+            {artistsContent.map((dataItem, index) => (
+              <Route path={getArtistPath(dataItem.name)} key={index}>
+                <ArtistPage
+                  name={dataItem.name}
+                  bio={dataItem.bio}
+                  picture={dataItem.picture}
+                  releases={dataItem.releases}
+                  links={dataItem.links}
+                />
+              </Route>
+            ))}
+            {getReleasesContent().map((content, index) => (
+              <Route path={getReleasePath(content.title)} key={index}>
+                <ReleasePage
+                  author={content.author}
+                  title={content.title}
+                  images={content.images}
+                  catNumber={content.catNumber}
+                  credits={content.credits}
+                  description={content.description}
+                  releaseDate={content.releaseDate}
+                  format={content.format}
+                  albumId={content.albumId}
+                  buyUrl={content.buyUrl}
+                />
+              </Route>
+            ))}
+            <Route path="/releases">
+              <ReleasesPage />
+            </Route>
+            <Route path="/info">
+              <InfoPage />
+            </Route>
+            <Route path="/artists">
+              <ArtistsPage />
+            </Route>
+            <Route path="/ui-system">
+              <UISystemPage />
+>>>>>>> Stashed changes
             </Route>
           ))}
           {getReleasesContent().map((content) => (
