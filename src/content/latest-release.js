68b8c6d2316b009d218175d0bfdf0cf810d74518
miguelsueiro <73364404+miguelsueiro@users.noreply.github.com>
2022-04-08 14:45:36 +0200

    Update latest-release.js

--- src/content/latest-release.js
+++ src/content/latest-release.js
@@ -1,5 +1,5 @@
 export const latestReleaseContent = {
-  id: 1,
+  id: 12,
   image: 'hyperchaotic-new-release.jpg',
   description: [
     'Huma «Hyperchaotic» explores chaos, theoretical physics, uncertainties and how order emerges out of the fog of the real. No apparent or understandable reason is required for this or anything else to simply, be.',
